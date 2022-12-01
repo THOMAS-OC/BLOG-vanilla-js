0707962417fd7850d091d1ba62ca415db491d87c
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-12-01 11:24:04 +0100

    Sauvegarde auto de la saisie

--- form.js
+++ form.js
@@ -1,5 +1,39 @@
 const saveArticle = document.querySelector('button[type="submit"]')
+const deleteInfo = document.querySelector('button[type="reset"]')
+const inputs = document.querySelectorAll("input")
 
+const autoSave = JSON.parse(localStorage.getItem("autoSave")) || false
+
+if (autoSave) {
+    console.log(autoSave);
+    document.querySelector("input[name='auteur']").value = autoSave.auteur
+    document.querySelector("input[name='img']").value = autoSave.pictureProfil
+    document.querySelector("input[name='categorie']").value = autoSave.categorie
+    document.querySelector("input[name='titre']").value = autoSave.titre
+}
+
+// Suppression des infos
+deleteInfo.addEventListener("click", () => {
+    localStorage.removeItem("autoSave")
+})
+
+// Sauvegarde auto
+inputs.forEach((input)=>{
+    input.addEventListener("change", () => {
+        let saveArticle = {
+            auteur : document.querySelector("input[name='auteur']").value,
+            pictureProfil : document.querySelector("input[name='img']").value,
+            categorie :document.querySelector("input[name='categorie']").value,
+            titre :document.querySelector("input[name='titre']").value,
+            contenu :document.querySelector("textarea[name='contenu']").value
+        }
+        localStorage.setItem("autoSave", JSON.stringify(saveArticle))
+        console.log(localStorage)
+    })
+
+})
+
+// Ajout de l'article
 saveArticle.addEventListener("click", (e) => {
     e.preventDefault()
     let newArticle = {

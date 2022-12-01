848f42c4d1a00d85a4b5c4b259b649f147cfb284
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-12-01 14:01:29 +0100

    Affichage dynamique de s articles

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 let posts = []
+const main = document.querySelector("main")
 
 for (let key of Object.keys(localStorage)) {
     
     if (key != "autoSave") {
-        console.log(key);
         let post = JSON.parse(localStorage.getItem(key))
         posts.push(post)
     }
 
 }
 
-console.log(posts);
+for (const iterator of posts) {
+    console.log(iterator);
+    main.innerHTML += `<article>
+    <img src="${iterator.pictureProfil}" alt="Photo de profil">
+    <h2> ${iterator.titre}</h2>
+    <h3> ${iterator.auteur} - date</h3>
+    <p> ${iterator.contenu}</p>            
+    <div>
+        <button class="delete">Supprimer</button>
+        <button class="update">Sauvegarder</button>
+    </div>
+
+    </article>
+    `
+}
